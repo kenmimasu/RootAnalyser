Fixed but in particles.py where set_four_momentum was losing the sign of pz.

RootAnalyser/particles.py
@@ -29,7 +29,7 @@ class Particle: # Base particle class
         self.ee = np.sqrt(self.modp**2+self.mass**2)
         self.px = pt*np.cos(phi) # px
         self.py = pt*np.sin(phi) # pyself.y = get_rapidity(et,pt)
-        self.pz = np.sqrt(self.modp**2-self.pt**2)
+        self.pz = np.sqrt(self.modp**2-self.pt**2)*abs(eta)/eta
         
     def get_y(self,eta,pt,mass):
         modp =  pt*np.cosh(eta)